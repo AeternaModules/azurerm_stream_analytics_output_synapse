77aaa4831a3f9d2712eacc6b5b6150099ca3b68f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,34 @@
-output "stream_analytics_output_synapses" {
-  description = "All stream_analytics_output_synapse resources"
-  value       = azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses
-  sensitive   = true
-}
 output "stream_analytics_output_synapses_database" {
-  description = "List of database values across all stream_analytics_output_synapses"
-  value       = [for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : v.database]
+  description = "Map of database values across all stream_analytics_output_synapses, keyed the same as var.stream_analytics_output_synapses"
+  value       = { for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : k => v.database }
 }
 output "stream_analytics_output_synapses_name" {
-  description = "List of name values across all stream_analytics_output_synapses"
-  value       = [for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : v.name]
+  description = "Map of name values across all stream_analytics_output_synapses, keyed the same as var.stream_analytics_output_synapses"
+  value       = { for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : k => v.name }
 }
 output "stream_analytics_output_synapses_password" {
-  description = "List of password values across all stream_analytics_output_synapses"
-  value       = [for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : v.password]
+  description = "Map of password values across all stream_analytics_output_synapses, keyed the same as var.stream_analytics_output_synapses"
+  value       = { for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : k => v.password }
   sensitive   = true
 }
 output "stream_analytics_output_synapses_resource_group_name" {
-  description = "List of resource_group_name values across all stream_analytics_output_synapses"
-  value       = [for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : v.resource_group_name]
+  description = "Map of resource_group_name values across all stream_analytics_output_synapses, keyed the same as var.stream_analytics_output_synapses"
+  value       = { for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : k => v.resource_group_name }
 }
 output "stream_analytics_output_synapses_server" {
-  description = "List of server values across all stream_analytics_output_synapses"
-  value       = [for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : v.server]
+  description = "Map of server values across all stream_analytics_output_synapses, keyed the same as var.stream_analytics_output_synapses"
+  value       = { for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : k => v.server }
 }
 output "stream_analytics_output_synapses_stream_analytics_job_name" {
-  description = "List of stream_analytics_job_name values across all stream_analytics_output_synapses"
-  value       = [for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : v.stream_analytics_job_name]
+  description = "Map of stream_analytics_job_name values across all stream_analytics_output_synapses, keyed the same as var.stream_analytics_output_synapses"
+  value       = { for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : k => v.stream_analytics_job_name }
 }
 output "stream_analytics_output_synapses_table" {
-  description = "List of table values across all stream_analytics_output_synapses"
-  value       = [for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : v.table]
+  description = "Map of table values across all stream_analytics_output_synapses, keyed the same as var.stream_analytics_output_synapses"
+  value       = { for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : k => v.table }
 }
 output "stream_analytics_output_synapses_user" {
-  description = "List of user values across all stream_analytics_output_synapses"
-  value       = [for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : v.user]
+  description = "Map of user values across all stream_analytics_output_synapses, keyed the same as var.stream_analytics_output_synapses"
+  value       = { for k, v in azurerm_stream_analytics_output_synapse.stream_analytics_output_synapses : k => v.user }
 }
 
